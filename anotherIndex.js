a805ac1de0407a2888fae7a455496c06f43ceca8
wudifeixue <wudifeixue@users.noreply.github.com>
2017-03-19 17:22:04 -0600

    add request handlers

---
+++ anotherIndex.js
@@ -0,0 +1,10 @@
+var logic = require("./logic");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+logic.start(router.route, handle);
