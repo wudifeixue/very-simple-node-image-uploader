179d6abe37080481f58a2ca228c8958159680590
wudifeixue <wudifeixue@users.noreply.github.com>
2017-03-20 06:11:11 -0600

    Finish the file upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,8 @@
 var querystring = require("querystring"),
-  fs = require("fs");
+  fs = require("fs"),
+  formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>'+
@@ -10,9 +11,10 @@ function start(response, postData) {
     'charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload">'+
+    '<input type="submit" value="Upload 文件" />'+
     '</form>'+
     '</body>'+
     '</html>';
@@ -22,15 +24,23 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent the text: " +
-  querystring.parse(postData).text);
-  response.end();
+  
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+    fs.renameSync(files.upload.path, "temp/test.gif");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image: <br/>");
+    response.write("<img src='show' />");
+    response.end();
+  });
+  
 }
 
-function show(response, postaData) {
+function show(response) {
   console.log("request handler 'show' was called.");
   fs.readFile("temp/test.gif", "binary", function(error, file) {
     if (error) {
